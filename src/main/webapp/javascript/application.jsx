2459f57d63a77ccee5350ea6dd3d3f48d3be24c4
James Kolean <jameskolean@yahoo.com>
2016-12-06 10:08:35 -0500

    this is a test 2

--- src/main/webapp/javascript/application.jsx
+++ src/main/webapp/javascript/application.jsx
@@ -1,7 +1,7 @@
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "React" }] */
 import React, { Component } from 'react'
 import Nav from './nav/nav'
-
+// this is a test 2
 export default class Application2 extends Component {
   render () {
     return (
